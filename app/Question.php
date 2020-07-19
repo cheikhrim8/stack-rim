3d097313636e73f2faf09b31f4b2d61cafef14ba
Cheikh Ahmed <cheikhrim8@gmail.com>
2020-07-19 20:14:05 +0000

    Initial Commit

---
+++ app/Question.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Question extends Model
+{
+
+    protected $guarded = [];
+
+    public function setTitleAttribute($value){
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value, '-');
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
